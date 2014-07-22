6e6a032846c575c562e0dfc0ea08e349b17543ab
davidjust <davidlow9999@gmail.com>
2014-07-22 17:37:58 +0700

    Add EmptyDataSource.

--- src/main/java/simple/escp/data/DataSources.java
+++ src/main/java/simple/escp/data/DataSources.java
@@ -1,5 +1,7 @@
 package simple.escp.data;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -8,26 +10,41 @@ import java.util.logging.Logger;
 /**
  * A factory class to create <code>DataSource</code>.
  */
+@SuppressWarnings("unchecked")
 public abstract class DataSources {
 
     private static final Logger LOG = Logger.getLogger("simple.escp");
+    private static EmptyDataSource emptyDataSource = new EmptyDataSource();
+    public static final List<DataSourceEntry> DATA_SOURCES;
+
+    static {
+        List<DataSourceEntry> newDataSource = new ArrayList<>();
+        newDataSource.add(new DataSourceEntry(Object.class, BeanDataSource.class));
+        newDataSource.add(new DataSourceEntry(Map.class, MapDataSource.class));
+        DATA_SOURCES = newDataSource;
+    }
 
     /**
      * Create a new <code>DataSource</code> from an <code>Object</code>.  This method will select the
      * appropriate <code>DataSouce</code> based on the class type of <code>Object</code>.
      *
-     * @param object the data source value.
+     * @param object the data source value. Passing <code>null</code> value will always return
+     *               an instance of <code>EmptyDataSource</code>.
      * @return an implementation of <code>DataSource</code>.
      */
-    @SuppressWarnings("unchecked")
     public static DataSource from(Object object) {
-        if (object instanceof Map) {
-            LOG.info("Creating new MapDataSource for [" + object + "]");
-            return new MapDataSource((Map) object);
-        } else {
-            LOG.info("Creating new BeanDataSource for [" + object + "]");
-            return new BeanDataSource(object);
+        if (object == null) {
+            return emptyDataSource;
         }
+        for (int i = DATA_SOURCES.size() - 1; i >= 0; i--) {
+            DataSourceEntry dataSourceEntry = DATA_SOURCES.get(i);
+            if (dataSourceEntry.support(object)) {
+                return dataSourceEntry.create(object);
+            }
+        }
+        LOG.severe("Can't create data source for [" + object + "] class [" + object.getClass() + "]");
+        throw new UnsupportedOperationException("No data source available for [" + object + "] class [" +
+            object.getClass() + "]");
     }
 
     /**
@@ -40,9 +57,7 @@ public abstract class DataSources {
     public static DataSource[] from(Object[] objects) {
         List<DataSource> sources = new ArrayList<>();
         for (Object o : objects) {
-            if (o != null) {
-                sources.add(from(o));
-            }
+            sources.add(from(o));
         }
         return sources.toArray(new DataSource[0]);
     }
@@ -56,6 +71,82 @@ public abstract class DataSources {
      */
     public static DataSource[] from(Map map, Object object) {
         return from(new Object[] {map, object});
+    }
+
+    /**
+     * Internal class used by <code>DataSource</code>.
+     */
+    public static class DataSourceEntry {
+
+        private Class supportedType;
+        private Class dataSourceType;
+
+        /**
+         * Create new instance of <code>DataSourceEntry</code>.
+         *
+         * @param supportedType class of object that can be handled by this data source.
+         * @param dataSourceType an implementation of <code>DataSource</code> that will be created by this entry.
+         */
+        public DataSourceEntry(Class supportedType, Class dataSourceType) {
+            this.supportedType = supportedType;
+            this.dataSourceType = dataSourceType;
+        }
+
+        /**
+         * Get the supported value type (in <code>Class</code>) that is supported by this <code>DataSource</code>.
+         *
+         * @return a <code>Class</code> that represent the type of value that can be handled by this
+         *         <code>DataSource</code>.
+         */
+        public Class getSupportedType() {
+            return supportedType;
+        }
+
+        /**
+         * Get the <code>DataSource</code> that will be created if a matching value was found.
+         *
+         * @return a <code>Class</code> that should be an implementation of <code>DataSource</code>.
+         */
+        public Class getDataSourceType() {
+            return dataSourceType;
+        }
+
+        /**
+         * Determine if the <code>DataSource</code> can be used for this <code>object</code>.  An <code>object</code>
+         * is considered as supported if it is an instance of <code>supportedType</code> or an instance of
+         * subclass of <code>supportedType</code>.
+         *
+         * @param object the object that contains value.
+         * @return <code>true</code> if this object can be used as value source for <code>DataSource</code>.
+         */
+        public boolean support(Object object) {
+            return supportedType.isAssignableFrom(object.getClass());
+        }
+
+        /**
+         * Create a new instance of <code>DataSource</code> from an <code>object</code>.
+         *
+         * @param object the object that contains value.
+         * @return an instance of <code>dataSourceType</code> constructed from <code>object</code>.
+         */
+        public DataSource create(Object object) {
+            if (!support(object)) {
+                throw new UnsupportedOperationException("[" + object + "] type [" + object.getClass() +
+                    "] is not supported by [" + dataSourceType + "]");
+            }
+            try {
+                Constructor constructor = dataSourceType.getConstructor(supportedType);
+                return (DataSource) constructor.newInstance(object);
+            } catch (NoSuchMethodException e) {
+                LOG.severe("Can't find constructor that accept [" + object.getClass().getName() + "] for [" +
+                    dataSourceType.getClass().getName());
+                throw new RuntimeException(e);
+            } catch (InvocationTargetException | InstantiationException | IllegalAccessException e) {
+                LOG.severe("Can't create data source for [" + object + "] class [" + object.getClass() + "]");
+                throw new RuntimeException(e);
+            }
+        }
+
     }
 
 }
